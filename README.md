5dbcac320d7983fb6eece03ebd1ce2ef39d2dbac
Oneil Pinnock <web.dev2145@gmail.com>
2020-12-27 09:11:04 -0500

    added requirements.txt file

---
+++ README.md
@@ -0,0 +1,16 @@
+README
+
+Pull data from billboard hot 100
+
+Use beautifulsoup to isolate song titles
+
+create spotify account 
+
+create spotify app
+
+copy credebtials and put in your program
+
+download spotipy module to python venv
+import spotipy module
+
+modules are in the requirements.txt file
--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
+from credentials import *
 
 #TODO 1: Scrape billboard top 100 website
 URL = 'https://www.billboard.com/charts/hot-100/'
 date = '2020-12-26'
-# date = input("Enter date to use for creating playlist (YYYY-MM-DD): ")
+date = input("Enter date to use for creating playlist (YYYY-MM-DD): ")
 
 response = requests.get(url=f'{URL}{date}')
 
@@ -15,6 +19,7 @@ songs_data = songs_soup.find_all(name='span', class_="chart-element__information
 
 # songs_data = songs_soup.findAll(name='span', {'class': ["chart-element__information__song", "text--truncate", "color--primary"]})
 
-print(songs_data)
+for song in songs_data:
+    print(song.get_text())
 
 #TODO 2: Create spotify playlist
