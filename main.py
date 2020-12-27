fd1611db9685e5f653c3c1f04ee2b0951f2f6dcc
Oneil Pinnock <web.dev2145@gmail.com>
2020-12-26 21:23:49 -0500

    Verified scraped data from billboard hot 100

---
+++ main.py
@@ -0,0 +1,12 @@
+import requests
+from bs4 import BeautifulSoup
+
+#TODO 1: Scrape billboard top 100 website
+URL = 'https://www.billboard.com/charts/hot-100/'
+
+date = input("Enter date to use for creating playlist (YYYY-MM-DD): ")
+
+response = requests.get(url=f'{URL}/data')
+
+print(response.text)
+#TODO 2: Create spotify playlist
